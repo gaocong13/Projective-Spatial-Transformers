735cdf2d34d527e70f19e127b7306daca0b0ca17
Cong Gao <cgao11@jhu.edu>
2020-03-23 11:34:37 -0400

    update grid generation function

--- ProSTGrid/ProSTGrid_cuda.cpp
+++ ProSTGrid/ProSTGrid_cuda.cpp
@@ -1,3 +1,7 @@
+/* This is the main c++ code of our ProST grid generator function.
+   Please cite our paper if you use this implementation: "Generalizing Spatial Transformers to Projective Geometry with Applications to 2D/3D Registration"
+   @Copyright: Cong Gao, the Johns Hopkins University. Email: cgao11@jhu.edu
+ */
 #include <ATen/ATen.h>
 #include <ATen/NativeFunctions.h>
 #include <torch/extension.h>
@@ -11,6 +15,65 @@
 #define CHECK_CONTIGUOUS(x) AT_ASSERTM(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
+/* ProST base grid generation CPU function */
+torch::Tensor ProST_grid_generator_5D_cpu_forward(
+  const torch::Tensor &theta, // pose parameter
+  int64_t N,        // batch size
+  int64_t C,        // projection channel, currently only supports 1
+  int64_t H,        // projection dim H
+  int64_t W,        // projection dim W
+  float dist_min,   // min distance from src to volume
+  float dist_max,   // max distance from src to volume
+  float src,        // source z-axis coordinate
+  float det,        // detector plane z-axis coordinate
+  float pix_spacing,// projection pixel spacing, currently only supports isotropic pixel
+  float step_size,  // ray casting sampling step size
+  bool align_corners)
+{
+  // Find source-to-detector distance
+  float src_det_len = abs(src - det);
+  // Create sampling range vector
+  auto range = torch::range(dist_min-step_size, dist_max+step_size, step_size, theta.options());
+  int64_t rc_pts = range.size(0);
+  // Initialize ProST grid variable
+  auto ProST_grid = torch::empty({N, H, W, rc_pts, 4}, theta.options());
+  // basic range vectors for future reference
+  auto tmp_range_x = range;
+  auto tmp_range_y = range;
+  auto tmp_range_z = range;
+  
+  float idx, idy, ray_len_idx, x_to_vec, y_to_vec, z_to_vec;
+  // For loop over batch size
+  for(int cntn = 0; cntn < N; cntn++){
+    // For loop over each pixel on 2D projection
+    for(int cntx = 0; cntx < H; cntx++){
+      // Current pixel x-coordinate in projection plane
+      idx = -pix_spacing*H/2 + cntx*pix_spacing;
+      for(int cnty = 0; cnty < W; cnty++){
+        // Current pixel y-coordinate in projection plane
+        idy = -pix_spacing*W/2 + cnty*pix_spacing;
+        // Length of ray-casting line connecting source to current pixel
+        ray_len_idx = sqrt(idx*idx + idy*idy + src_det_len*src_det_len);
+        // x, y, z to ray-casting line ratio
+        x_to_vec = idx / ray_len_idx;
+        y_to_vec = idy / ray_len_idx;
+        z_to_vec = src_det_len / ray_len_idx;
+        // Apply to basic range vector
+        tmp_range_x = range * x_to_vec;
+        tmp_range_y = range * y_to_vec;
+        tmp_range_z = src - range * z_to_vec;
+        // Fill in values
+        ProST_grid.select(0, cntn).select(0, cntx).select(0, cnty).select(-1, 0).copy_(tmp_range_x);
+        ProST_grid.select(0, cntn).select(0, cntx).select(0, cnty).select(-1, 1).copy_(tmp_range_y);
+        ProST_grid.select(0, cntn).select(0, cntx).select(0, cnty).select(-1, 2).copy_(tmp_range_z);
+        ProST_grid.select(0, cntn).select(0, cntx).select(0, cnty).select(-1, 3).fill_(1);
+      }
+    }
+  }
+  return ProST_grid;
+}
+
+/* ProST base grid generation CUDA driver function */
 torch::Tensor ProST_grid_generator_5D_cuda_forward(
   const torch::Tensor &theta,
   int64_t N,
@@ -27,7 +90,12 @@ torch::Tensor ProST_grid_generator_5D_cuda_forward(
   ); 
 
 torch::Tensor ProST_grid_generator_forward(const torch::Tensor &theta, torch::IntArrayRef size, float dist_min, float dist_max, float src, float det, float pix_spacing, float step_size, bool align_corners) {
+  /* Check device type */
+  if(theta.type().is_cuda()){
     return ProST_grid_generator_5D_cuda_forward(theta, size[0], size[1], size[2], size[3], dist_min, dist_max, src, det, pix_spacing, step_size, align_corners);
+  }else{
+    return ProST_grid_generator_5D_cpu_forward(theta, size[0], size[1], size[2], size[3], dist_min, dist_max, src, det, pix_spacing, step_size, align_corners);
+  }
 }
   
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
--- ProSTGrid/ProSTGrid_cuda_kernel.cu
+++ ProSTGrid/ProSTGrid_cuda_kernel.cu
@@ -1,15 +1,20 @@
+/* This is the CUDA kernel of our ProST grid generator function.
+   Please cite our paper if you use this implementation: "Generalizing Spatial Transformers to Projective Geometry with Applications to 2D/3D Registration"
+   @Copyright: Cong Gao, the Johns Hopkins University. Email: cgao11@jhu.edu
+ */
+
 #include <torch/extension.h>
 
 #include <cuda.h>
 #include <cuda_runtime.h>
 
 #include <vector>
-#include <math.h> 
+#include <math.h>
 
 namespace{
   template <typename scalar_t>
-  __global__ void make_base_grid_5D_kernel(
-    torch::PackedTensorAccessor<scalar_t,5,torch::RestrictPtrTraits,size_t> base_grid,
+  __global__ void make_ProST_grid_5D_kernel(
+    torch::PackedTensorAccessor<scalar_t,5,torch::RestrictPtrTraits,size_t> ProST_grid,
     torch::PackedTensorAccessor<scalar_t,1,torch::RestrictPtrTraits,size_t> range,
     int64_t N,
     int64_t H,
@@ -31,7 +36,7 @@ namespace{
       float tmp_range_y;
       float tmp_range_z;
       float idx, idy;
-      if (r < base_grid.size(1) && c < base_grid.size(2)){
+      if (r < ProST_grid.size(1) && c < ProST_grid.size(2)){
         idx = -pix_spacing*H/2 + r*pix_spacing;
         idy = -pix_spacing*W/2 + c*pix_spacing;
         ray_len_idx = sqrt(idx*idx + idy*idy + src_det_len*src_det_len);
@@ -43,17 +48,17 @@ namespace{
             tmp_range_x = range[rc_ind] * x_to_vec;
             tmp_range_y = range[rc_ind] * y_to_vec;
             tmp_range_z = src - range[rc_ind] * z_to_vec;
-            base_grid[bn][r][c][rc_ind][0] = tmp_range_x;
-            base_grid[bn][r][c][rc_ind][1] = tmp_range_y;
-            base_grid[bn][r][c][rc_ind][2] = tmp_range_z;
-            base_grid[bn][r][c][rc_ind][3] = 1;
+            ProST_grid[bn][r][c][rc_ind][0] = tmp_range_x;
+            ProST_grid[bn][r][c][rc_ind][1] = tmp_range_y;
+            ProST_grid[bn][r][c][rc_ind][2] = tmp_range_z;
+            ProST_grid[bn][r][c][rc_ind][3] = 1;
           }
         }
       }
     }
 }
 
-torch::Tensor base_grid_generator_5D_cuda_forward(
+torch::Tensor ProST_grid_generator_5D_cuda_forward(
   const torch::Tensor &theta,
   int64_t N,
   int64_t C,
@@ -67,24 +72,23 @@ torch::Tensor base_grid_generator_5D_cuda_forward(
   float step_size,
   bool align_corners
   ) {
-    //Calculate maximum ray casting sampling points
     float src_det_len = abs(src - det);
-    //Create base grid
+
     auto range = torch::range(dist_min-step_size, dist_max+step_size, step_size, theta.options());
     const auto rc_pts = range.size(0);
-    auto base_grid = torch::empty({N, H, W, rc_pts, 4}, theta.options());
+    auto ProST_grid = torch::empty({N, H, W, rc_pts, 4}, theta.options());
     auto tmp_range_x = range;
     auto tmp_range_y = range;
     auto tmp_range_z = range;
-
+    // Pre-defined GPU block dimensions
     const int dimx = 32;
     const int dimy = 32;
     const dim3 dimBlock(dimx, dimy);
     const dim3 dimGrid((int)ceil(H/dimBlock.x), (int)ceil(W/dimBlock.y));
 
-    AT_DISPATCH_FLOATING_TYPES(base_grid.type(), "base_grid_generator_5D_cuda_forward", ([&] {
-    make_base_grid_5D_kernel<scalar_t><<<dimGrid, dimBlock>>>(
-        base_grid.packed_accessor<scalar_t, 5, torch::RestrictPtrTraits,size_t>(),
+    AT_DISPATCH_FLOATING_TYPES(ProST_grid.type(), "ProST_grid_generator_5D_cuda_forward", ([&] {
+    make_ProST_grid_5D_kernel<scalar_t><<<dimGrid, dimBlock>>>(
+        ProST_grid.packed_accessor<scalar_t, 5, torch::RestrictPtrTraits,size_t>(),
         range.packed_accessor<scalar_t, 1, torch::RestrictPtrTraits,size_t>(),
         N,
         H,
@@ -94,5 +98,5 @@ torch::Tensor base_grid_generator_5D_cuda_forward(
         src_det_len
       );
       }));
-    return base_grid.view({N, -1, 4});
+    return ProST_grid.view({N, -1, 4});
 }
